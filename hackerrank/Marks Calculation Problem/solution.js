448d093aafaf8026872d44a64885d19a68539405
Dev AKS <devaks24k@gmail.com>
2020-05-03 21:41:34 +0530

    Hackerrank -> Marks Calculation Problem added.

---
+++ hackerrank/Marks Calculation Problem/solution.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let aliceScroe = 0;
+    let bobScore = 0;
+    for ( let num in a) {
+        if ( a[num] > b[num]) {
+            ++aliceScroe;
+        } else if ( a[num] < b[num]) {
+            ++bobScore;
+        }
+    }
+    return [aliceScroe, bobScore];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
